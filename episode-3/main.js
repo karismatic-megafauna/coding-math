009a1d5013da525c40c4a1655e32cc5dc52b1bfb
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-12-28 23:51:43 -0800

    Add episode 2

--- episode-3/main.js
+++ episode-3/main.js
@@ -3,12 +3,20 @@ window.onload = function() {
     context = canvas.getContext('2d'),
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight;
+ 
+  context.translate(0, height / 2);
+  context.scale(1,-1);
   
-  for( var i = 1; i < 100; i += 1) {
-    context.beginPath();
-    context.moveTo(Math.random() * width, Math.random() * height);
-    context.lineTo(Math.random() * width, Math.random() * height);
-    context.stroke();
+  for( var angle = 0; angle < Math.PI * 2; angle += .01) {
+    var x = angle * 200,
+        y = Math.sin(angle) * 200;
+
+    context.fillStyle = 'black';
+    context.fillRect(x, y, 5, 5);
+
+    y = Math.cos(angle) * 200;
+    context.fillStyle = 'red';
+    context.fillRect(x, y, 5, 5);
   }
 
 };

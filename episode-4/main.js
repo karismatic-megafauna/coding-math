c24629e6d20cdfe2e93c38a80f93df38e68f4897
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-12-30 17:53:54 -0800

    I think this works?

--- episode-4/main.js
+++ episode-4/main.js
@@ -6,19 +6,18 @@ window.onload = function() {
  
   var centerY = height * .5,
       centerX = width * .5,
-      base = 100,
-      offset = 50,
-      speed = 0.011,
+      offset = height * .4,
+      speed = 0.111,
       angle = 0;
 
   render();
 
   function render() {
-    var radius = base + Math.sin(angle) * offset;
+    var x = centerX + Math.sin(angle) * offset;
 
     context.clearRect(0, 0, width, height);
     context.beginPath();
-    context.arc(centerX, centerY, radius, 0, Math.PI * 2, false);
+    context.arc(x, centerY, 50, 0, Math.PI * 2, false);
     context.fill();
 
     angle += speed;

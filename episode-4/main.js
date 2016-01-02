9e6ca1f2928cc007c4db70dfccfedf9fc56c3542
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-01-01 22:33:03 -0800

    Make it an ellipse!

--- episode-4/main.js
+++ episode-4/main.js
@@ -6,7 +6,8 @@ window.onload = function() {
 
   var centerY = height / 2,
       centerX = width / 2,
-      radius = 200,
+      xRadius = 200,
+      yRadius = 400,
       speed = 0.01,
       angle = 0,
       x, y;
@@ -15,8 +16,8 @@ window.onload = function() {
 
   function render() {
     context.clearRect(0, 0, width, height);
-    x = centerX + Math.cos(angle) * radius;
-    y = centerY + Math.sin(angle) * radius;
+    x = centerX + Math.cos(angle) * xRadius;
+    y = centerY + Math.sin(angle) * yRadius;
     context.beginPath();
     context.arc(x, y, 20, 0, Math.PI * 2, false);
     context.arc(centerX, centerY, 40, 0, Math.PI * 2, false);

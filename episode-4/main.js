a3b5b92db8d00f4ea773c4ece367df75e3e15b39
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-01-02 09:12:07 -0800

    Circles on a circle

--- episode-4/main.js
+++ episode-4/main.js
@@ -6,27 +6,19 @@ window.onload = function() {
 
   var centerY = height / 2,
       centerX = width / 2,
-      xRadius = 200,
-      yRadius = 400,
-      xSpeed = 0.09,
-      ySpeed = 0.05,
-      xAngle = 0,
-      yAngle = 0,
+      radius = 200,
+      speed = 0.05,
+      angle = 0,
+      numObjects = 20,
+      slice = Math.PI * 2 / numObjects,
       x, y;
 
-  render();
-
-  function render() {
-    context.clearRect(0, 0, width, height);
-    x = centerX + Math.cos(xAngle) * xRadius;
-    y = centerY + Math.sin(yAngle) * yRadius;
+  for ( var i = 0; i < numObjects; i += 1 ) {
+    angle = i * slice;
+    x = centerX + Math.cos(angle) * radius;
+    y = centerY + Math.sin(angle) * radius;
     context.beginPath();
     context.arc(x, y, 20, 0, Math.PI * 2, false);
-    context.arc(centerX, centerY, 40, 0, Math.PI * 2, false);
     context.fill();
-
-    xAngle += xSpeed;
-    yAngle += ySpeed;
-    requestAnimationFrame(render);
   }
 };

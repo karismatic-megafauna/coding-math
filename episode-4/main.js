20b01b4d5e0f1201ac2cb385f253798388fea15c
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-01-01 22:43:25 -0800

    Lissajous Curves

--- episode-4/main.js
+++ episode-4/main.js
@@ -8,22 +8,25 @@ window.onload = function() {
       centerX = width / 2,
       xRadius = 200,
       yRadius = 400,
-      speed = 0.01,
-      angle = 0,
+      xSpeed = 0.09,
+      ySpeed = 0.05,
+      xAngle = 0,
+      yAngle = 0,
       x, y;
 
   render();
 
   function render() {
     context.clearRect(0, 0, width, height);
-    x = centerX + Math.cos(angle) * xRadius;
-    y = centerY + Math.sin(angle) * yRadius;
+    x = centerX + Math.cos(xAngle) * xRadius;
+    y = centerY + Math.sin(yAngle) * yRadius;
     context.beginPath();
     context.arc(x, y, 20, 0, Math.PI * 2, false);
     context.arc(centerX, centerY, 40, 0, Math.PI * 2, false);
     context.fill();
 
-    angle += speed;
+    xAngle += xSpeed;
+    yAngle += ySpeed;
     requestAnimationFrame(render);
   }
 };

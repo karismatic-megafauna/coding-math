8f043f00162319e8184a21f59a9738ef2eafef91
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-01-02 17:16:47 -0800

    Rotating arrow

--- episode-5/main.js
+++ episode-5/main.js
@@ -2,13 +2,41 @@ window.onload = function() {
   var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
     width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight;
+    height = canvas.height = window.innerHeight,
+    arrowX = width / 2,
+    arrowY = height / 2,
+    dx, dy,
+    angle = 0;
+
+    render();
+
+    function render() {
+      context.clearRect(0, 0, width, height);
+
+      context.save();
+      context.translate(arrowX, arrowY);
+      context.rotate(angle);
+
+      // DRAW THE ARROW
+      context.beginPath();
+      context.moveTo(20,0);
+      context.lineTo(-20,0);
+      context.moveTo(20,0);
+      context.lineTo(10,-10);
+      context.moveTo(20,0);
+      context.lineTo(10,10);
+      context.stroke();
+
+      context.restore();
+      requestAnimationFrame(render);
+      console.log('Bitches ', angle);
+    }
+
+    document.body.addEventListener("mousemove", function(event){
+      dx = event.clientX - arrowX;
+      dy = event.clientY - arrowY;
+      angle = Math.atan2(dy , dx);
+    });
   
-  for( var i = 1; i < 100; i += 1) {
-    context.beginPath();
-    context.moveTo(Math.random() * width, Math.random() * height);
-    context.lineTo(Math.random() * width, Math.random() * height);
-    context.stroke();
-  }
 
 };

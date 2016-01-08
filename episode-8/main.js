5ecaba8705805a6e1fc13d22b52ceffbc34976dd
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-01-08 08:13:25 -0800

    Basic movement with the vector lib

--- episode-8/main.js
+++ episode-8/main.js
@@ -1,22 +1,47 @@
-var v1 = vector.create(10, 5);
-// get'ers work
-console.log("Get'ers work:")
-console.log(v1.getX(), v1.getY());
-console.log(v1.getAngle());
-console.log(v1.getLength());
+// var v1 = vector.create(10, 5);
+// // get'ers work
+// console.log("Get'ers work:")
+// console.log(v1.getX(), v1.getY());
+// console.log(v1.getAngle());
+// console.log(v1.getLength());
+//
+// // Adding works
+// var v2 = vector.create(1, 1);
+// var v3 = v1.add(v2);
+// console.log('Adding works:')
+// console.log(v3.getX(), v3.getY());
+//
+// // Subtracting works
+// var v4 = v3.subtract(v2);
+// console.log('Subtracting works:')
+// console.log(v4.getX(), v4.getY());
+//
+// // Multiplication works
+// var v5 = v1.multiply(2);
+// console.log('Multiplication works:');
+// console.log(v5.getX(), v5.getY());
 
-// Adding works
-var v2 = vector.create(1, 1);
-var v3 = v1.add(v2);
-console.log('Adding works:')
-console.log(v3.getX(), v3.getY());
+window.onload = function() {
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var width = canvas.width = window.innerWidth;
+  var height = canvas.height = window.innerHeight;
+  var position = vector.create(700,400);
+  var velocity = vector.create(3,3);
 
-// Subtracting works
-var v4 = v3.subtract(v2);
-console.log('Subtracting works:')
-console.log(v4.getX(), v4.getY());
+  velocity.setLength(1);
+  velocity.setAngle( (3 * Math.PI)/2 );
 
-// Multiplication works
-var v5 = v1.multiply(2);
-console.log('Multiplication works:');
-console.log(v5.getX(), v5.getY());
+  update();
+
+  function update() {
+    context.clearRect(0,0,width,height);
+
+    position.addTo(velocity);
+    context.beginPath();
+    context.arc(position.getX(), position.getY(), 10, 0, Math.PI * 2, false);
+    context.fill();
+
+    requestAnimationFrame(update);
+  }
+};

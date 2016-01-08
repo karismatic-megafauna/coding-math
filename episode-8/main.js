c98d99b7297d0efbc1b0a5e06f9c850dc21ff804
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-01-08 08:37:18 -0800

    Added a particle object, make it explode like BB

--- episode-8/main.js
+++ episode-8/main.js
@@ -26,21 +26,25 @@ window.onload = function() {
   var context = canvas.getContext('2d');
   var width = canvas.width = window.innerWidth;
   var height = canvas.height = window.innerHeight;
-  var position = vector.create(700,400);
-  var velocity = vector.create(3,3);
+  var particles = [];
+  numParticles = 100;
 
-  velocity.setLength(1);
-  velocity.setAngle( (3 * Math.PI)/2 );
+  for( var i = 0; i < numParticles; i++){
+    particles.push(particle.create(width/2,height/2,Math.random()* 4 +1, Math.random() * Math.PI * 2));
+  }
 
   update();
 
   function update() {
     context.clearRect(0,0,width,height);
 
-    position.addTo(velocity);
-    context.beginPath();
-    context.arc(position.getX(), position.getY(), 10, 0, Math.PI * 2, false);
-    context.fill();
+    for(var i = 0; i < numParticles; i++) {
+      var p = particles[i];
+      p.update();
+      context.beginPath();
+      context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
+      context.fill();
+    }
 
     requestAnimationFrame(update);
   }

5903b4e1a0b64256099aeb58216ff8160950629b
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-03 09:35:20 -0800

    Add acceleration particle mutation test

--- test/particle-test.js
+++ test/particle-test.js
@@ -2,6 +2,7 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 import { particle } from '../js/particle';
+import { vector } from '../js/vector';
 
 describe('Particle Library', () => {
   describe('Can create create vectors', () => {
@@ -29,14 +30,24 @@ describe('Particle Library', () => {
       expect(p1.gravity.getY()).to.equal(20);
     });
   });
-  describe('Arithmetic works', () => {
+  describe('Particle mutations', () => {
     // Methods to test:
     // accelerate(accel) {
+    it('increases the velocity by a specified amount', () => {
+      const p1 = particle.create(4, 4, 30, Math.PI / 6, 20);
+      const v2 = vector.create(10, 5);
+      const originalX = p1.velocity.getX();
+      const originalY = p1.velocity.getY();
+      // mutate p1
+      p1.accelerate(v2);
+
+      expect(originalX + 10).to.eql(p1.velocity.getX());
+      expect(originalY + 5).to.eql(p1.velocity.getY());
+    });
     // update() {
     // angleTo(p2) {
     // distanceTo(p2) {
     // gravitateTo(p2) {
-    it('increases the velocity by a specified amount');
     it('updates the velocity and position');
     it('gets the angle between two particles');
     it('gets the distance between two particles');

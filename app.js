269aeab75d5d44b468908dd849b12d0f7981b776
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-02 20:31:58 -0800

    Add missing forward slash

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.get('/', function build(request, response) {
-  response.sendfile(__dirname + 'build/index.html');
+  response.sendfile(__dirname + '/build/index.html');
 }).listen(port, function log() {
   console.log('listening on port: ', port);
 });

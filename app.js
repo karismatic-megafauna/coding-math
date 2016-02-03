93b459d21557f30c7399be2d0ee12a77e15fb25b
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-02 20:14:47 -0800

    Conform to express API

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
-var app = express.createServer();
+var app = express();
 
-app.get('/', function(request, response) {
+app.get('/', function build(request, response) {
   response.sendfile(__dirname + 'build/index.html');
-}).listen(port);
-
-console.log('listening on port: ', port);
+}).listen(port, function log() {
+  console.log('listening on port: ', port);
+});

d93f62de695bc1e22c51863f4b010d345af1b087
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-02 23:24:23 -0800

    I really hope this works

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
+
 app.get('/', function build(request, response) {
-  response.sendfile(__dirname + '/build/index.html');
-  response.sendfile(__dirname + '/build/bundle.js');
+  app.use('/build', express.static(__dirname + '/build'));
+  response.sendFile(__dirname + '/build/index.html');
 }).listen(port, function log() {
   console.log('listening on port: ', port);
 });

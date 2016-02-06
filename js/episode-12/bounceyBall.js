a78cc96622999a60b54c19d51798cf2b6f723dd2
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-06 13:48:25 -0800

    Add velocity scaler

--- js/episode-12/bounceyBall.js
+++ js/episode-12/bounceyBall.js
@@ -21,6 +21,7 @@ export const bounceyBall = {
       0.1
     );
     p.radius = 50;
+    p.bounce = -0.9;
 
     function update() {
       context.clearRect(0, 0, width, height);
@@ -40,19 +41,19 @@ export const bounceyBall = {
 
       if (p.position.getX() + p.radius > width) {
         p.position.setX(width - p.radius);
-        p.velocity.setX(p.velocity.getX() * -1);
+        p.velocity.setX(p.velocity.getX() * p.bounce);
       }
       if (p.position.getX() - p.radius < 0) {
         p.position.setX(p.radius);
-        p.velocity.setX(p.velocity.getX() * -1);
+        p.velocity.setX(p.velocity.getX() * p.bounce);
       }
       if (p.position.getY() + p.radius > height) {
         p.position.setY(height - p.radius);
-        p.velocity.setY(p.velocity.getY() * -1);
+        p.velocity.setY(p.velocity.getY() * p.bounce);
       }
       if (p.position.getY() - p.radius < 0) {
         p.position.setY(p.radius);
-        p.velocity.setY(p.velocity.getY() * -1);
+        p.velocity.setY(p.velocity.getY() * p.bounce);
       }
 
       if (switcher.on) {

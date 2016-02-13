afe7e997da2a986d8d1360cc387f1999f62016e4
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-12 19:26:38 -0800

    Draws a sin wave to the width of the screen and a set number of points

--- js/episode-2/sinAndCos.js
+++ js/episode-2/sinAndCos.js
@@ -1,33 +1,59 @@
 // TODO: animate in an interesting way or remove
+const switcher = { on: false };
+let frameId;
+
 export const sinAndCos = {
   tearDown() {
-    // nothing to do yet
+    switcher.on = false;
+    cancelAnimationFrame(frameId);
   },
   setUp() {
+    switcher.on = true;
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
+    const width = canvas.width = window.innerWidth;
     const height = canvas.height = window.innerHeight;
-    const randNum = (Math.floor(Math.random() * 300) + 100);
+    // const randNum = (Math.floor(Math.random() * 300) + 100);
 
     context.translate(0, height / 2);
-    context.scale(1, -1);
+    // context.scale(1, -1);
 
-    for (let angle = 0; angle < Math.PI * 2; angle += 0.01) {
-      // Some fun randoms!
-      // const x = angle * (Math.floor(Math.random() * 300) + 100);
-      // const x = angle * Math.random() * 300;
-      const x = angle * 300;
-      let y = 0;
+    // what does a wave need?
+    // Amplitude
+    // cycles, default Math.PI * 2
+    // width, ( hard code to width of screen) 
+    function printWave() {
+    }
 
-      // create Sin
-      y = Math.sin(angle) * randNum;
+    //     function render() {
+    //       context.clearRect(0, 0, width, height);
+    // go through the unit circle and render the whole thing
+    // then move the starting point and do it again
+    // y oscillates between 0 and 1
+    //
+    // PROBLEM: x is the position on the screen AND the position on the circle
+    // SOLUTION: abstract out the width of the drawing
+    // Calculate the step based off of width and unit circle
+    let points = 200;
+    let end = Math.PI * 2;
+    let stepNum = end / points;
+    let stepLength = (width / points);
+    let start = 0;
+    const amp = 400;
+    let y = 0;
+    let x = 0;
+    for (start; start < end; start += stepNum) {
+      x = x + stepLength;
+      y = Math.sin(start) * amp;
       context.fillStyle = 'black';
-      context.fillRect(x, y, 5, 5);
-
-      // create Cos
-      y = Math.cos(angle) * randNum;
-      context.fillStyle = 'red';
+      // context.fillText(Math.sin(start).toFixed(2), x+10, y + 10); 
+      // context.fillText(x.toFixed(2), x+10, y + 30); 
       context.fillRect(x, y, 5, 5);
     }
+    // if (switcher.on) {
+    //   frameId = requestAnimationFrame(render);
+    // }
+    // }
+    // render();
   },
 };
--- js/episode-3/growingCircle.js
+++ js/episode-3/growingCircle.js
@@ -20,7 +20,7 @@ export const growingCircle = {
     const speed = 0.011;
     let angle = 0;
 
-    function draw() {
+    function render() {
       const radius = base + Math.sin(angle) * offset;
 
       context.clearRect(0, 0, width, height);
@@ -30,10 +30,10 @@ export const growingCircle = {
 
       angle += speed;
       if (switcher.on) {
-        frameId = requestAnimationFrame(draw);
+        frameId = requestAnimationFrame(render);
       }
     }
 
-    draw();
+    render();
   },
 };

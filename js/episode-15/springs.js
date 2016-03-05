1bab8cb95f628863297a4faf8c48fdabcb521bd3
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-03-04 19:10:21 -0800

    Gravity spring!

--- js/episode-15/springs.js
+++ js/episode-15/springs.js
@@ -51,10 +51,74 @@ export const springOne = {
       );
       context.fill();
 
-      // context.beginPath();
-      // context.moveTo(weight.position.getX(), weight.position.getY());
-      // context.lineTo(springPoint.getX(), springPoint.getY());
-      // context.stroke();
+      context.beginPath();
+      context.moveTo(weight.position.getX(), weight.position.getY());
+      context.lineTo(springPoint.getX(), springPoint.getY());
+      context.stroke();
+
+      if (switcher.on) {
+        frameId = requestAnimationFrame(update);
+      }
+    }
+
+    update();
+  },
+};
+
+export const springTwo = {
+  tearDown() {
+    switcher.on = false;
+    cancelAnimationFrame(frameId);
+  },
+  setUp() {
+    switcher.on = true;
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const width = canvas.width = window.innerWidth;
+    const height = canvas.height = window.innerHeight;
+    const springPoint = vector.create(width / 2, height / 2);
+    const weight = particle.create(
+      Math.random() * width,
+      Math.random() * height,
+      50,
+      Math.random() * Math.PI * 2,
+      0.5
+    );
+    const springLength = 100;
+    const k = 0.1;
+
+    weight.radius = 20;
+    weight.friction = 0.95;
+
+    document.body.addEventListener('mousemove', function(event) {
+      springPoint.setX(event.clientX);
+      springPoint.setY(event.clientY);
+    });
+    function update() {
+      context.clearRect(0, 0, width, height);
+
+      const distance = springPoint.subtract(weight.position);
+      distance.setLength(distance.getLength() - springLength);
+      const springForce = distance.multiply(k);
+
+      weight.velocity.addTo(springForce);
+      weight.update();
+
+      context.beginPath();
+      context.arc(
+        weight.position.getX(),
+        weight.position.getY(),
+        weight.radius,
+        0,
+        Math.PI * 2,
+        false
+      );
+      context.fill();
+
+      context.beginPath();
+      context.moveTo(weight.position.getX(), weight.position.getY());
+      context.lineTo(springPoint.getX(), springPoint.getY());
+      context.stroke();
 
       if (switcher.on) {
         frameId = requestAnimationFrame(update);
--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ import { bounceyBall } from './episode-12/bounceyBall';
 import { volcano } from './episode-12/volcano';
 import { friction } from './episode-13/friction';
 import { collisionDetection } from './episode-14/collisionDetection';
-import { springOne } from './episode-15/springs';
+import { springOne, springTwo } from './episode-15/springs';
 
 const sidebar = document.querySelector('#sidebar');
 
@@ -29,6 +29,7 @@ const components = {
   'Friction': friction,
   'Collision Detection': collisionDetection,
   'Mouse Spring': springOne,
+  'Mouse Spring 2': springTwo,
 };
 
 let currentComponentName = 'Mouse Spring';

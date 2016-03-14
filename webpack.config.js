85992f8904d05cb9e68d874cda2bde8ccfbc2978
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-03-14 08:30:27 -0700

    Add css packages for css-modules

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
           presets: ['es2015', 'react'],
         },
       },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+      },
     ],
   },
   plugins: [

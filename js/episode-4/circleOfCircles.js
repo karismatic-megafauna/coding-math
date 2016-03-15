03d3de25a303780b24762b50bcfb00b70920ec71
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-03-14 18:34:31 -0700

    All working again!

--- js/episode-4/circleOfCircles.js
+++ js/episode-4/circleOfCircles.js
@@ -8,10 +8,7 @@ export const circleOfCircles = {
     const width = canvas.width = window.innerWidth;
     const height = canvas.height = window.innerHeight;
     const randNum = (Math.floor(Math.random() * 200) + 100);
-    const circleArrangementRadius = window.counterState;
-    const individualCircleRadii = window.counterState;
     const randNum2 = (Math.floor(Math.random() * 30) + 10);
-		const cState = Counter.getState();
     const centerY = height / 2;
     const centerX = width / 2;
     const numObjects = 20;
@@ -22,10 +19,10 @@ export const circleOfCircles = {
 
     for (let i = 0; i < numObjects; i += 1) {
       angle = i * slice;
-      x = centerX + Math.cos(angle) * circleArrangementRadius;
-      y = centerY + Math.sin(angle) * circleArrangementRadius;
+      x = centerX + Math.cos(angle) * randNum;
+      y = centerY + Math.sin(angle) * randNum;
       context.beginPath();
-      context.arc(x, y, individualCircleRadii, 0, Math.PI * 2, false);
+      context.arc(x, y, randNum2, 0, Math.PI * 2, false);
       context.fill();
     }
   },

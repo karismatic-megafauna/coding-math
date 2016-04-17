eda0946d27c21060235ece9cd16a8067cc5905df
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-04-17 00:19:08 -0700

    It works! Now i need to pass all the components around like this

--- js/episode-1/RandomLines.jsx
+++ js/episode-1/RandomLines.jsx
@@ -5,18 +5,19 @@ export class RandomLines extends Component {
     super(props);
   }
 
-  componentWillMount() {
-    // const canvas = document.getElementById('canvas');
-    // const context = canvas.getContext('2d');
-    // const width = canvas.width = window.innerwidth;
-    // const height = canvas.height = window.innerHeight;
+  componentDidMount() {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const width = canvas.width = window.innerWidth;
+    const height = canvas.height = window.innerHeight;
 
-    // for (let i = 1; i < 100; i += 1) {
-    //   context.beginPath();
-    //   context.moveTo(Math.random() * width, Math.random() * height);
-    //   context.lineTo(Math.random() * width, Math.random() * height);
-    //   context.stroke();
-    // }
+    for (let i = 1; i < 100; i += 1) {
+      context.beginPath();
+      context.moveTo(Math.random() * width, Math.random() * height);
+      context.lineTo(Math.random() * width, Math.random() * height);
+      context.stroke();
+    }
+    debugger;
   }
   componentWillUnmount() {
     console.log('component unmounted');
